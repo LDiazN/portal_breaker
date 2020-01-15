3a886530d5f6bee50899c7ca6072b94e2c7a3da5
xdgorgola <pedro18fe@gmail.com>
2020-01-15 12:18:58 -0400

    Removed unused code

--- portal_brick/Ball/BallMovement.gd
+++ portal_brick/Ball/BallMovement.gd
@@ -7,10 +7,5 @@ func _physics_process(delta):
 	var coll = move_and_collide(velocity * speed * delta);
 	if (coll != null):
 		var normal : Vector2 = coll.normal;
-		if (normal == Vector2.UP or normal == Vector2.DOWN or
-			normal == Vector2.RIGHT or normal == Vector2.LEFT):
-				var reflex : Vector2 = velocity.bounce(normal);
-				velocity = reflex;
-		else:
-			var reflex : Vector2 = velocity.bounce(normal);
-			velocity = reflex;
+		var reflex : Vector2 = velocity.bounce(normal);
+		velocity = reflex;
