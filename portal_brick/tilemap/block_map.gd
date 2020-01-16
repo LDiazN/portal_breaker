cb216cdf2dd12d8c6ad3493dc8f0c866de15e366
Luis Diaz <ldiazn98@gmail.com>
2020-01-15 22:47:50 -0400

    Added two-block tilemap, and block map objects

---
+++ portal_brick/tilemap/block_map.gd
@@ -0,0 +1,14 @@
+extends TileMap
+#In order to use this block map object you have to connect 
+#the ball node to it.
+
+#_on_brick_found(pos : Vector2)
+#	Deletes the tile in position "pos". This function is the handler 
+#	for "brick_found" signal in the ball.
+#	param:
+#		pos: Contact point between thhe ball and some tile
+#		in this tilemap
+func _on_brick_found(pos : Vector2) -> void:
+	var tile_pos : Vector2 = world_to_map(pos)
+	set_cellv(tile_pos, -1);
+
